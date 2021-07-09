e4daacc6d3f7cb345ba0b0cf806a7708a663a8ce
kksumm123 <kksumm123@gmail.com>
2021-07-09 19:36:35 +0900

    애니메이션 커브를 활용한 캐릭터점프 완료

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,18 +12,52 @@ public class Player : MonoBehaviour
     {
         moustPointer = GetComponent<Transform>();
     }
-
+    Plane plain = new Plane(new Vector3(0, 1, 0), 0);
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
+        Move();
+        Jump();
+    }
+
+    public AnimationCurve jumpYac;
+    void Jump()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            moustPointer.position = hit.point;
-            float distnace = Vector3.Distance(hit.point, transform.position);
+            StartCoroutine(JumpCo());
+        }
+    }
+
+    [SerializeField] float jumpYMult = 1;
+    private IEnumerator JumpCo()
+    {
+        yield return null;
+        float jumpStartTime = Time.time;
+        float jumpDuration = jumpYac[jumpYac.length - 1].time;
+        float jumpEndTime = jumpStartTime + jumpDuration;
+        float sumEvaluateTime = 0;
+        while (Time.time < jumpEndTime)
+        {
+            float y = jumpYac.Evaluate(sumEvaluateTime);
+            y *= jumpYMult;
+            transform.Translate(0, y, 0);
+            yield return null;
+            sumEvaluateTime += Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter = 0.0f;
+        if (plain.Raycast(ray, out enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            moustPointer.position = hitPoint;
+            float distnace = Vector3.Distance(hitPoint, transform.position);
             if (distnace > moveAbleDistance)
             {
-                var dir = hit.point - transform.position;
+                var dir = hitPoint - transform.position;
                 dir.Normalize();
                 transform.Translate(dir * speed * Time.deltaTime);
             }
