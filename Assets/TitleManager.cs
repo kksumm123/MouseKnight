82b66208fd2172774d342c0d7ee967087965ef93
kksumm123 <kksumm123@gmail.com>
2021-07-14 19:11:39 +0900

    한줄로 리팩토링

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -13,10 +13,8 @@ public class TitleManager : MonoBehaviour
         // 검은 화면에서 밝게 한다.
         blackScreen = GameObject.Find("PersistCanvas").transform.Find("BlackScreen").GetComponent<CanvasGroup>();
         blackScreen.gameObject.SetActive(true);
-        blackScreen.DOFade(0, 0.7f).OnComplete(()=>
-        {
-            blackScreen.gameObject.SetActive(false);
-        });
+        blackScreen.DOFade(0, 1.7f).OnComplete(() =>
+            blackScreen.gameObject.SetActive(false));
 
         // 뉴 게임 누르면 스테이지 1로드
         Button button = GameObject.Find("TitleCanvas").transform.Find("Button").GetComponent<Button>();
@@ -29,9 +27,7 @@ public class TitleManager : MonoBehaviour
     {
         // 점점 어둡게
         blackScreen.gameObject.SetActive(true);
-        blackScreen.DOFade(1, 0.7f).OnComplete(() =>
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(loadSceneName);
-        });
+        blackScreen.DOFade(1, 1.7f).OnComplete(() =>
+            UnityEngine.SceneManagement.SceneManager.LoadScene(loadSceneName));
     }
 }
