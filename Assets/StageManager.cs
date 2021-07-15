0ac2f892b4fbd3471771b7c3c5ff43ad617a26ff
kksumm123 <kksumm123@gmail.com>
2021-07-15 16:40:34 +0900

    아 맞다 커밋 1. StageManager.cs 생성, 주석작성 2. SpawnPoint.cs 생성

---
+++ Assets/StageManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//스테이지에서 발행하는 모든 이벤트 관리
+//스테이지 부서지면 같이 부서짐
+//1. 플레이어 로드게임 시작시까지 플레이어 대기시킴
+//2. 스테이지 시작시 화면 밝아지게
+//3. 몬스터 로드
+public class StageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
