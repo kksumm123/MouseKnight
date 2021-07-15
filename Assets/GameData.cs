e7ac991f073f82c733dd005b9d8427f38218ae81
kksumm123 <kksumm123@gmail.com>
2021-07-15 20:16:47 +0900

    GameData 씬을 넘어가도 부셔지지않도록

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public class StageInfo
+{
+    public int stageID;
+    public string titleString;
+    public int rewardXP;
 
+}
 public class GameData : MonoBehaviour
 {
-    
+    public static GameData instance;
+    public List<StageInfo> stageInfos;
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 }
